a33522520dd4044600a71a7299a6ff02f3362c56
tommy <2130194@bart.ca>
2023-02-02 17:09:33 -0500

    First Commit

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    // Distance the player needs to get to the object to interact with it.
+    public float InteractRadius = 3f;
+    public Transform interactionTransform;
+    public Transform interactionWithTrees;
+    public Transform interactionWithRocks;
+    public Transform interactionWithFishes;
+
+    bool isFocus = false;
+    Transform player;
+
+    bool hasInteracted = false;
+
+    public virtual void Interact()
+    {
+        // This method is meant to be overwritten
+        Debug.Log("Interacting with" + transform.name);
+
+        if (interactionTransform == interactionWithTrees)
+        {
+            //Woodcutting();
+        }
+        
+        if (interactionTransform == interactionWithRocks)
+        {
+            //Mining();
+        }
+        
+        if (interactionTransform == interactionWithFishes)
+        {
+            //Fishing();
+        }
+    }
+
+    void Update()
+    {
+        if (isFocus && !hasInteracted)
+        {
+            float distance = Vector3.Distance(player.position, interactionTransform.position);
+            if (distance <= InteractRadius)
+            {
+                Interact();
+                hasInteracted = true;
+            }
+        }
+    }
+
+    public void OnFocused(Transform playerTransform)
+    {
+        isFocus = true;
+        player = playerTransform;
+        hasInteracted = false;
+    }
+
+    public void OnDefocused()
+    {
+        isFocus = false;
+        player = null;
+        hasInteracted = false;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (interactionTransform == null)
+            interactionTransform = transform;
+
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(interactionTransform.position, InteractRadius);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+
+public class PlayerController : MonoBehaviour
+{
+    public Interactable focus;
+    
+    public LayerMask movementMask;
+
+    Camera cam;
+    PlayerMotor motor;
+
+    void Start()
+    {
+        cam = Camera.main;
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100, movementMask))
+            {
+                Debug.Log("We hit " + hit.collider.name + " " + hit.point);
+                // Move player to location clicked
+                motor.MoveToPoint(hit.point);
+
+                RemoveFocus();
+            }
+        }
+         
+        if (Input.GetMouseButtonDown(1)) 
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+               Interactable interactable =  hit.collider.GetComponent<Interactable>();
+                if (interactable != null)
+                {
+                    SetFocus(interactable);
+                    
+                }
+            }
+        }
+    }
+
+    void SetFocus(Interactable newFocus)
+    {
+        if (newFocus != focus)
+        { 
+            if (focus != null)
+                focus.OnDefocused();
+
+            focus = newFocus;
+            motor.FollowTarget(newFocus);
+        }
+
+        newFocus.OnFocused(transform);
+    }
+
+    void RemoveFocus()  
+    {
+        if (focus != null)
+            focus.OnDefocused();
+
+        focus = null;
+        motor.StopFollowingTarget();
+    }
+}
